d76733ad4fed4d3419ff64c5659f874d075d8ac6
Karthick <32698132+KarthickRavi594@users.noreply.github.com>
2023-02-27 15:47:18 +0530

    Add files via upload

---
+++ const.js
@@ -0,0 +1,3 @@
+module.exports = { 
+    API_Key: "f0da691f16a740cd8893fa2d6209cf12"
+}
---
+++ index.js
@@ -0,0 +1,63 @@
+const axios = require("axios");
+const API = require("./const");
+const fs = require("fs");
+var bodyParser = require("body-parser");
+const port = process.env.PORT || 3456;
+var express = require("express");
+const assembly = axios.create({
+  baseURL: "https://api.assemblyai.com/v2",
+  headers: {
+    authorization: API.API_Key,
+    "transfer-encoding": "chunked",
+  },
+});
+const file = "./Audio/sample-2.mp3";
+var app = express();
+app.listen(port, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Running in ", port);
+  }
+});
+app.use(bodyParser.json());
+app.get("/uploadStatus", (req, response) => {
+  try {
+    fs.readFile(file, (err, data) => {
+      if (err) return console.error(err);
+
+      assembly
+        .post("/upload", data)
+        .then((res) => {
+          response.send({ upload_url: res.data.upload_url });
+        })
+        .catch((err) => {
+          console.log("Err", err);
+        });
+    });
+  } catch (err) {}
+});
+app.post("/submit", (request, response) => {
+  try {
+    console.log("submit => ", request.body);
+    assembly
+      .post("/transcript", {
+        audio_url: request.body.url,
+      })
+      .then((res) => {
+        response.send({ submittedData: res.data });
+      })
+      .catch((err) => console.error(err));
+  } catch (err) {}
+});
+app.post("/transcription", (request, response) => {
+  try {
+    console.log("transcription => ", request.body);
+    assembly
+      .get(`/transcript/${request.body.id}`)
+      .then((res) => {
+        response.send({ uploadedData: res.data });
+      })
+      .catch((err) => console.error(err));
+  } catch (err) {}
+});
